b2e986afbc8f1409ceb4d7dcecc72dde01f648b1
tamer1an <trybynenko@gmail.com>
2014-01-08 03:44:35 +0200

    step1

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,14 +1,36 @@
+//package hello;
+//
+//        import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//        import org.springframework.boot.SpringApplication;
+//        import org.springframework.context.annotation.ComponentScan;
+//
+//@ComponentScan
+//@EnableAutoConfiguration
+//public class Application {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(Application.class, args);
+//    }
+//}
+
 package hello;
 
-        import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-        import org.springframework.boot.SpringApplication;
-        import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
 
-@ComponentScan
+@Controller
 @EnableAutoConfiguration
 public class Application {
 
-    public static void main(String[] args) {
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
 }
